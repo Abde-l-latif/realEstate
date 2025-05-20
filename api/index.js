09c04ea114010ab52c5355ea96d27c57339bf136
abdellatif <outegnitabdellatif@gmail.com>
2025-05-20 20:07:39 +0100

    i change the version of express to 4.18.2 because of a problem in route

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const AuthRoute = require("./routes/AuthRoute.js");
 const ListingRoute = require("./routes/ListingRoute.js");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const fs = require("fs");
 DOTENV.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -19,7 +20,8 @@ mongoose
     console.log(error);
   });
 
-const dirName = path.resolve(__dirname);
+const dirName = path.resolve();
+const frontEndPath = path.join(dirName, "realEstate", "dist");
 
 app.listen(5000, () => {
   console.log("SERVER IS RUNNING  !!");
@@ -29,11 +31,34 @@ app.use("/api/user", UserRoute);
 app.use("/api/auth", AuthRoute);
 app.use("/api/listing", ListingRoute);
 
-app.use(express.static(path.join(dirName, "realEstate", "dist")));
-
+// app.use(express.static(path.join(dirName, "/realEstate/dist")));
+app.use(express.static(frontEndPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(dirName, "realEstate", "dist", "index.html"));
+  res.sendFile(path.join(frontEndPath, "index.html"));
 });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(dirName, "realEstate", "dist", "index.html"));
+// });
+
+// const staticPath = path.join(dirName, "realEstate", "dist");
+
+// if (fs.existsSync(staticPath)) {
+//   app.use(express.static(staticPath));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(staticPath, "index.html"));
+//   });
+// } else {
+//   console.error("Frontend build not found at:", staticPath);
+//   // Optional: Only enable in development
+//   if (process.env.NODE_ENV === "development") {
+//     app.get("*", (req, res) => {
+//       res
+//         .status(500)
+//         .send("Frontend not built - run 'npm run build' in realEstate folder");
+//     });
+//   }
+// }
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
